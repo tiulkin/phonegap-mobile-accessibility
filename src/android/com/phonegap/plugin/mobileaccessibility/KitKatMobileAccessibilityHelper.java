83f2ba4695c63d414119e7977d82d6acf9a77f1e
tiulkin <oleg@tulkin.ru>
2014-05-31 15:02:09 +0000

    Update KitKatMobileAccessibilityHelper.java

--- src/android/com/phonegap/plugin/mobileaccessibility/KitKatMobileAccessibilityHelper.java
+++ src/android/com/phonegap/plugin/mobileaccessibility/KitKatMobileAccessibilityHelper.java
@@ -33,68 +33,6 @@ public class KitKatMobileAccessibilityHelper extends
         JellyBeanMobileAccessibilityHelper {
     protected CaptioningManager mCaptioningManager;
     protected CaptioningChangeListener mCaptioningChangeListener;
-    protected TouchExplorationStateChangeListener mTouchExplorationStateChangeListener;
+    //protected TouchExplorationStateChangeListener mTouchExplorationStateChangeListener;
 
-    @Override
-    public void initialize(MobileAccessibility mobileAccessibility) {
-        super.initialize(mobileAccessibility);
-        mCaptioningManager = (CaptioningManager) mobileAccessibility.cordova.getActivity().getSystemService(Context.CAPTIONING_SERVICE);
-    }
-
-    @Override
-    public boolean isScreenReaderRunning() {
-        return mAccessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_BRAILLE | AccessibilityServiceInfo.FEEDBACK_SPOKEN).size() > 0;
-    }
-
-    @Override
-    public boolean isClosedCaptioningEnabled() {
-        return mCaptioningManager.isEnabled();
-    }
-
-    @Override
-    public boolean isTouchExplorationEnabled() {
-        return mAccessibilityManager.isTouchExplorationEnabled();
-    }
-
-    @Override
-    public void addStateChangeListeners() {
-        super.addStateChangeListeners();
-        if (mCaptioningChangeListener == null) {
-            mCaptioningChangeListener = new CaptioningChangeListener() {
-                /** @hide */
-                @Override
-                public void onEnabledChanged(boolean enabled) {
-                    onCaptioningEnabledChanged(enabled);
-                }
-            };
-        }
-        mCaptioningManager.addCaptioningChangeListener(mCaptioningChangeListener);
-
-        if (mTouchExplorationStateChangeListener == null) {
-            mTouchExplorationStateChangeListener = new InternalTouchExplorationStateChangeListener();
-        }
-        mAccessibilityManager.addTouchExplorationStateChangeListener(mTouchExplorationStateChangeListener);
-    }
-
-    @Override
-    public void removeStateChangeListeners() {
-        super.removeStateChangeListeners();
-        if (mCaptioningChangeListener != null) {
-            mCaptioningManager.removeCaptioningChangeListener(mCaptioningChangeListener);
-            mCaptioningChangeListener = null;
-        }
-        if (mTouchExplorationStateChangeListener != null) {
-            mAccessibilityManager.removeTouchExplorationStateChangeListener(mTouchExplorationStateChangeListener);
-            mTouchExplorationStateChangeListener = null;
-        }
-    }
-
-    protected class InternalTouchExplorationStateChangeListener
-        implements TouchExplorationStateChangeListener {
-
-        @Override
-        public void onTouchExplorationStateChanged(boolean enabled) {
-            mMobileAccessibility.onTouchExplorationStateChanged(enabled);
-        }
-    }
 }
